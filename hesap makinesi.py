9b14b82a0d83c25cf30ecc7f7bedc422899bb8ee
tevfikoktay <okhoabc@gmail.com>
2024-08-11 12:18:27 +0300

    Update hesap makinesi.py
    Except ValueError under the islem==4 causes error. This should be worked.

--- hesap makinesi.py
+++ hesap makinesi.py
@@ -7,35 +7,38 @@ print("""********************************
 4. Bölme İşlemi
 ****************************************
 """)
+
 while True:
-    islem = input("İşlem Seçiniz (Çıkış için 'q' ya basalım): ")
-    if islem == 'q':
-        print("Çıkılıyor...")
-        quit()
-    elif islem == "1":
-        print("------Toplama İşlemi------")
-        sayi1 = int(input("1.Sayıyı Giriniz: "))
-        sayi2 = int(input("2.Sayıyı Giriniz: "))
-        print("{}  +   {}    =  {}".format(sayi1, sayi2, sayi1+sayi2))
-    elif islem == "2":
-        print("------Çıkarma İşlemi------")
-        sayi1 = float(input("1.Sayıyı Giriniz: "))
-        sayi2 = float(input("2.Sayıyı Giriniz: "))
-        print("{}  -   {}    =  {}".format(sayi1, sayi2, sayi1-sayi2))
-    elif islem == "3":
-        print("------Çarpma İşlemi------")
-        sayi1 = float(input("1.Sayıyı Giriniz: "))
-        sayi2 = float(input("2.Sayıyı Giriniz: "))
-        print("{}  x   {}    =  {}".format(sayi1, sayi2, sayi1*sayi2))
-    elif islem == "4":
-        print("------Bölme İşlemi------")
         try:
-            sayi1 = int(input("1.Sayıyı Giriniz: "))
-            sayi2 = int(input("2.Sayıyı Giriniz: "))
-            print("{}  /   {}    =  {:.2f}".format(sayi1, sayi2, sayi1/sayi2))
+            islem = input("İşlem Seçiniz (Çıkış için 'q' ya basalım): ")
+            if islem == "q":
+                print("Çıkılıyor")
+                quit()
+            elif islem == "1":
+                print("-----Toplama İşlemi-----")
+                sayi1 = float(input("1.Sayıyı Giriniz: "))
+                sayi2 = float(input("2.Sayıyı Giriniz: "))
+                print("{} + {}  = {} ".format(sayi1,sayi2,sayi1+sayi2))
+            elif islem == "2":
+                print("-----Çıkarma İşlemi-----")
+                sayi1 = float(input("1.Sayıyı Giriniz: "))
+                sayi2 = float(input("2.Sayıyı Giriniz: "))
+                print("{} + {}  = {} ".format(sayi1, sayi2, sayi1 - sayi2))
+            elif islem == "3":
+                print("------Çarpma İşlemi------")
+                sayi1 = float(input("1.Sayıyı Giriniz: "))
+                sayi2 = float(input("2.Sayıyı Giriniz: "))
+                print("{}  x   {}    =  {}".format(sayi1, sayi2, sayi1 * sayi2))
+            elif islem == "4":
+                print("------Bölme İşlemi------")
+                sayi1 = int(input("1.Sayıyı Giriniz: "))
+                sayi2 = int(input("2.Sayıyı Giriniz: "))
+                print("{}  /   {}    =  {:.2f}".format(sayi1, sayi2, sayi1 / sayi2))
+
+            else:
+                print("Geçersiz Seçenek...")
+
         except ZeroDivisionError:
-            print("Bir sayıyı 0'a bölemezsiniz!")
+            print("Bir sayıyı 0'a bölemezsin!")
         except ValueError:
-            print("Lütfen sadece sayı girin!")
-    else:
-        print("Geçersiz Seçenek...")
+            print("Lütfen sadece bir sayı giriniz")
